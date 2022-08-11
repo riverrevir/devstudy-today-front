aa76ad1ee8ba51a82d9bc55cf0eed393cf3919b8
ekim <eunkukkim1992@gmail.com>
2022-08-11 16:39:00 +0900

    calendar page, moment, react-big-calendar

---
+++ devstudy/src/component/unit/calendar/calendar.tsx
@@ -0,0 +1,67 @@
+import * as S from "./calendar-css";
+import { momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import { useCallback, useMemo, useState } from "react";
+
+export default function Calendar() {
+  const localizer = momentLocalizer(moment);
+
+  const [events, setEvents] = useState<any[]>([]);
+
+  const handleSelectSlot = useCallback(
+    ({ start, end }) => {
+      const title = window.prompt("Add new events");
+      if (title) {
+        setEvents((prev) => [...prev, { start, end, title }]);
+      }
+    },
+    [setEvents]
+  );
+
+  const onClickRemoveEvent = (event: any) => {
+    console.log("event:", event);
+    const newEvents = events.filter((el) => el.title !== event.title);
+    setEvents([...newEvents, events]);
+  };
+
+  const { defaultDate, scrollToTime } = useMemo(
+    () => ({
+      defaultDate: new Date(),
+      scrollToTime: new Date(2000, 1, 1, 6),
+    }),
+    []
+  );
+
+  const eventStyleGetter = (event: any) => {
+    console.log("eventGetter:", event.hexColor);
+    // const backgroundColor = "#" + event.hexColor;
+    const style = {
+      backgroundColor: "khaki",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "black",
+      border: "0px",
+      display: "block",
+    };
+    return {
+      style,
+    };
+  };
+
+  return (
+    <S.Wrapper>
+      <S.ScheduleCalendar
+        localizer={localizer}
+        events={events}
+        selectable={true}
+        views={["month", "week", "day"]}
+        defaultDate={defaultDate}
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={onClickRemoveEvent}
+        scrollToTime={scrollToTime}
+        eventPropGetter={eventStyleGetter}
+      />
+    </S.Wrapper>
+  );
+}
