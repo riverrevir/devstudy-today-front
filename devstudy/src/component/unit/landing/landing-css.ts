29fd627606d9aeb9511d0495e25e6ff7a0f18efa
ekim <eunkukkim1992@gmail.com>
2022-08-22 23:24:22 +0900

    feature-ek-landing clone update

--- devstudy/src/component/unit/landing/landing-css.ts
+++ devstudy/src/component/unit/landing/landing-css.ts
@@ -1,29 +1,153 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
   margin: 90px auto 0px auto;
 `;
 
-export const TitleWrapper = styled.header`
-  width: 100%;
-  height: 40px;
-  position: sticky;
-  top: 75px;
-`;
-
-export const Title = styled.h3`
-  padding: 4px 16px;
-`;
-
 export const ContentWrapper = styled.section`
   width: 100%;
   height: 40vh;
-  background: lightcoral;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleAnimation = keyframes`
+  from {
+    opacity: 0
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const Title = styled.h1`
+  height: 10vh;
+  line-height: 70px;
+  animation: ${TitleAnimation} 4s ease-out infinite;
+`;
+
+export const SecondTitle = styled.div`
+  font-size: 20px;
+  height: 60px;
+  line-height: 50px;
+`;
+
+export const ThirdTitle = styled.div`
+  font-size: 20px;
+  height: 60px;
+  line-height: 50px;
 `;
 
 export const CardWrapper = styled.section`
   width: 100%;
   height: 70vh;
-  background: lightcyan;
+  background: #efeff4;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const FirstCardOuterWrapper = styled.div`
+  width: 380px;
+  height: 320px;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const FirstCardWrapper = styled.div`
+  position: relative;
+  border: 2px solid #000;
+  width: 308px;
+  height: 282px;
+  border-radius: 8px;
+  padding: 10px;
+  :hover {
+    cursor: pointer;
+    width: 380px;
+    height: 320px;
+    transition: width 0.3s, height 0.3s;
+  }
+`;
+
+export const FirstCardTitle = styled.p`
+  font-size: 20px;
+  padding: 20px;
+  border-bottom: 3px solid #000;
+`;
+
+export const FirstCardInfo = styled.p`
+  padding: 10px;
+  font-size: 28px;
+`;
+
+export const FirstCardList = styled.ul`
+  background: lightcoral;
+`;
+
+export const SecondCardOuterWrapper = styled.div`
+  width: 380px;
+  height: 320px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const SecondCardWrapper = styled.div`
+  position: relative;
+  border: 2px solid #000;
+  width: 308px;
+  height: 282px;
+  border-radius: 8px;
+  padding: 10px;
+  :hover {
+    cursor: pointer;
+    width: 380px;
+    height: 320px;
+    transition: width 0.3s, height 0.3s;
+  }
+`;
+
+export const SecondCardTitle = styled.p`
+  font-size: 20px;
+  padding: 20px;
+  border-bottom: 3px solid #000;
+`;
+
+export const SecondCardInfo = styled.p`
+  padding: 10px;
+  font-size: 28px;
+`;
+
+export const ThirdCardOuterWrapper = styled.div`
+  width: 380px;
+  height: 320px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ThirdCardWrapper = styled.div`
+  position: relative;
+  border: 2px solid #000;
+  width: 308px;
+  height: 282px;
+  border-radius: 8px;
+  padding: 10px;
+  :hover {
+    cursor: pointer;
+    width: 380px;
+    height: 320px;
+    transition: width 0.3s, height 0.3s;
+  }
+`;
+
+export const ThirdCardTitle = styled.p`
+  font-size: 20px;
+  padding: 20px;
+  border-bottom: 3px solid #000;
+`;
+
+export const ThirdCardInfo = styled.p`
+  padding: 10px;
+  font-size: 28px;
 `;
--- devstudy/src/component/unit/landing/landing.tsx
+++ devstudy/src/component/unit/landing/landing.tsx
@@ -3,18 +3,45 @@ import * as S from "./landing-css";
 export default function Landing() {
   return (
     <S.Wrapper>
-      <S.TitleWrapper>
-        <S.Title>devStudy</S.Title>
-      </S.TitleWrapper>
       <S.ContentWrapper>
-        <div>2.0</div>
-        <div>더 새로워진 코드캠프</div>
-        <div>오프라인을 넘어 온라인까지</div>
+        <S.Title>devStudy과 함께 공부해요!</S.Title>
+        <S.SecondTitle>더 새로워진 devStudy</S.SecondTitle>
+        <S.ThirdTitle>언제 어디서든!</S.ThirdTitle>
       </S.ContentWrapper>
       <S.CardWrapper>
-        <div>card 1</div>
-        <div>card 2</div>
-        <div>card 3</div>
+        <S.FirstCardOuterWrapper>
+          <S.FirstCardWrapper>
+            <S.FirstCardTitle>내가 원하는 스택만 확실하게</S.FirstCardTitle>
+            <S.FirstCardInfo>온라인 클래스</S.FirstCardInfo>
+            <S.FirstCardList>
+              <li>오프라인의 커리큘럼과 멘토링 시스템 그대로 제공</li>
+              <li>지속적으로 업데이트되는 코드캠프 자체 콘텐츠로 학습</li>
+              <li>하루 9시간 멘토상주, 기다림 없는 즉각 피드백</li>
+            </S.FirstCardList>
+          </S.FirstCardWrapper>
+        </S.FirstCardOuterWrapper>
+        <S.SecondCardOuterWrapper>
+          <S.SecondCardWrapper>
+            <S.SecondCardTitle>어디서나 커리어 점프</S.SecondCardTitle>
+            <S.SecondCardInfo>코드캠프 온라인</S.SecondCardInfo>
+            <ul>
+              <li>오프라인의 커리큘럼과 멘토링 시스템 그대로 제공</li>
+              <li>지속적으로 업데이트되는 코드캠프 자체 콘텐츠로 학습</li>
+              <li>하루 9시간 멘토상주, 기다림 없는 즉각 피드백</li>
+            </ul>
+          </S.SecondCardWrapper>
+        </S.SecondCardOuterWrapper>
+        <S.ThirdCardOuterWrapper>
+          <S.ThirdCardWrapper>
+            <S.ThirdCardTitle>생생한 현장감</S.ThirdCardTitle>
+            <S.ThirdCardInfo>코드캠프 오프라인</S.ThirdCardInfo>
+            <ul>
+              <li>오프라인의 커리큘럼과 멘토링 시스템 그대로 제공</li>
+              <li>지속적으로 업데이트되는 코드캠프 자체 콘텐츠로 학습</li>
+              <li>하루 9시간 멘토상주, 기다림 없는 즉각 피드백</li>
+            </ul>
+          </S.ThirdCardWrapper>
+        </S.ThirdCardOuterWrapper>
       </S.CardWrapper>
     </S.Wrapper>
   );
