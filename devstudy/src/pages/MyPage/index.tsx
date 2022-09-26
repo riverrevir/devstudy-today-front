69300e16ffebcc1c08b77b1ba654074ae5ade17a
NEARworld <artemismars2@outlook.com>
2022-09-26 21:25:43 +0900

    feat: basic html structure

--- devstudy/src/pages/MyPage/index.tsx
+++ devstudy/src/pages/MyPage/index.tsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 
 function MyPage() {
-  return <MyPageWrapper></MyPageWrapper>
+  return <MyPageWrapper>
+    <SideMenuWrapper>
+
+    </SideMenuWrapper>
+    <FormWrapper>
+      <MyPageForm>
+
+      </MyPageForm>
+    </FormWrapper>
+  </MyPageWrapper>
 }
 
 export default MyPage;
 
 const MyPageWrapper = styled.div`
+
+`
+const SideMenuWrapper = styled.div`
+
+`
+const FormWrapper = styled.div`
+
+`
+const MyPageForm = styled.form`
 
 `
