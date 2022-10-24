20090e81f15aa139976b47e1fcaec2fe51fa7ee8
NEARworld <artemismars2@outlook.com>
2022-10-24 19:03:28 +0900

    fix: prettier formatting for MyPageForm component

--- devstudy/src/pages/MyPage/MyPageForm.tsx
+++ devstudy/src/pages/MyPage/MyPageForm.tsx
@@ -5,71 +5,70 @@ import ProfileUpdateInput from "./ProfileUpdateInput";
 const Inputs = [
   {
     id: 1,
-    type: 'text',
-    title: '이름',
-    formType: 'username'
+    type: "text",
+    title: "이름",
+    formType: "username",
   },
   {
     id: 2,
-    type: 'email',
-    title: '이메일',
-    notice: '인증필요',
-    formType: 'email'
+    type: "email",
+    title: "이메일",
+    notice: "인증필요",
+    formType: "email",
   },
   {
     id: 3,
-    type: 'text',
-    title: '휴대전화',
-    formType: 'phone'
+    type: "text",
+    title: "휴대전화",
+    formType: "phone",
   },
   {
     id: 4,
-    type: 'password',
-    title: '비밀번호',
-    formType: 'password'
-  }
+    type: "password",
+    title: "비밀번호",
+    formType: "password",
+  },
 ] as const;
 
 function MyPageForm() {
-  return <MyPageFormContainer>
-    <Header>
-      <h3>기본 정보</h3>
-      <Separator />
-    </Header>
-    <Form>
-      {
-        Inputs.map((item) =>
+  return (
+    <MyPageFormContainer>
+      <Header>
+        <h3>기본 정보</h3>
+        <Separator />
+      </Header>
+      <Form>
+        {Inputs.map((item) => (
           <Row key={item.id}>
             <InputTitle>{item.title}</InputTitle>
             <InputContainer>
               <ProfileUpdateInput type={item.type} formType={item.formType} />
-              {item.type === 'email' ? <InputButton>이메일 전송</InputButton> : null}
-              <InputButton type='button'>변경</InputButton>
+              {item.type === "email" ? (
+                <InputButton>이메일 전송</InputButton>
+              ) : null}
+              <InputButton type="button">변경</InputButton>
             </InputContainer>
           </Row>
-        )
-      }
-    </Form>
-  </MyPageFormContainer>
+        ))}
+      </Form>
+    </MyPageFormContainer>
+  );
 }
 export default MyPageForm;
 
 const MyPageFormContainer = styled.div`
   /* background-color: yellow; */
   margin: 30px;
-`
-const Header = styled.div`
-`
+`;
+const Header = styled.div``;
 const Separator = styled.hr`
   border-top: 2px black solid;
-`
-const Form = styled.form`
-`
-const Row = styled.div`
-`
+`;
+const Form = styled.form``;
+const Row = styled.div``;
 const InputTitle = styled.h5`
   margin-bottom: 0;
-`
+`;
 const InputContainer = styled.div`
   margin-top: 10px;
   height: 35px;
@@ -77,7 +76,7 @@ const InputContainer = styled.div`
   /* align-items: stretch; */
   gap: 10px;
 
-  @media screen and (max-width: 1670px){
+  @media screen and (max-width: 1670px) {
     display: grid;
     height: auto;
     & > button {
@@ -90,7 +89,7 @@ const InputContainer = styled.div`
       width: 100%;
     }
   }
-`
+`;
 const InputButton = styled.button`
   margin: 0;
   /* height: 100%; */
@@ -102,4 +101,4 @@ const InputButton = styled.button`
   &:hover {
     background-color: #dedede;
   }
-`
+`;
